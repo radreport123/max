c61bf3701db3b217164c155c074ab9ef2bf9166c
Asahan Sahan <asahansahan@Asahans-MacBook-Pro-2.local>
2023-02-05 19:35:55 -0500

    fifteen commit

--- util/database.js
+++ util/database.js
@@ -5,7 +5,7 @@ let _db;
 
 
 const mongoConnect = (callback) => {
-  mongoclient.connect('mongodb+srv://mac41:macbookpro123@cluster0.hque2fi.mongodb.net/?retryWrites=true&w=majority')
+  mongoclient.connect('mongodb://localhost:27017')
     .then(client => {
       console.log('connected');
       _db = client.db()
